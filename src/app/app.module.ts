30ae1e5a70ac74fb974f286f187932c9c56ee1b5
dgoalla <49921997+dgoalla@users.noreply.github.com>
2020-11-18 20:51:54 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthModule } from './auth/auth.module';
@@ -23,7 +22,6 @@ import { OrdersComponent } from './orders/orders.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProfileComponent,
     CreateUserComponent,
     CreateProductComponent,
     ManageProductsComponent,
